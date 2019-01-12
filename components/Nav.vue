15662cfd2dd2b4f267ddc14ec7a9efc099ae00d3
mariomarine <rwbleo@gmail.com>
2019-01-12 08:17:42 -0700

    Start of Nav Layout

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,9 +1,22 @@
 <template>
-	<ul>
-		<li><nuxt-link to="/"> Home </nuxt-link></li>
-		<li><nuxt-link to="/about"> About </nuxt-link></li>
-		<li><nuxt-link to="/thoughts"> Thoughts </nuxt-link></li>
-		<li><nuxt-link to="/contact"> Contact Us </nuxt-link></li>
-	</ul>
+	<nav>
+		<nuxt-link to="/"> Home </nuxt-link>
+		<nuxt-link to="/about"> About </nuxt-link>
+		<img class="logo" src="../UB.png" alt="Unbridled Thoughts Logo" />
+		<nuxt-link to="/thoughts"> Thoughts </nuxt-link>
+		<nuxt-link to="/contact"> Contact Us </nuxt-link>
+	</nav>
 </template>
+
+<style>
+	nav {
+		margin: 0;
+		padding: 0;
+		display: grid;
+		grid-template-columns: repeat(5, 1fr);
+	}
+	.logo {
+		height: 50px;
+	}
+</style>
 
